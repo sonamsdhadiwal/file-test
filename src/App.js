61fede48f00b72f2fa75486eafc8bad7c5034e10
sonamsd <ssdhadiwal.it@gmail.com>
2021-10-10 08:50:20 -0500

    fixing build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './config/theme';
+import Home from './components/pages/Home';
+import Routes from './Routes'
+import Navbar from './Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Navbar />
+   <Routes />
+            <Home/>
+        </MuiThemeProvider>
+    );
+};
 
 export default App;
